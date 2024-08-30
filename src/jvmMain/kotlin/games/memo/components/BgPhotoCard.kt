7538328360b0396e720631c8c066f883aeab7bac
timoleon <tilamprinos@gmail.com>
2024-08-30 18:35:20 +0300

    Add random colors to memo cards

--- src/jvmMain/kotlin/games/memo/components/BgPhotoCard.kt
+++ src/jvmMain/kotlin/games/memo/components/BgPhotoCard.kt
@@ -9,11 +9,15 @@ import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import games.hideAndChess.model.HideAndChessCell
+import ui.theme.*
 
 @Composable
 fun BgPhotoCard(
@@ -22,15 +26,26 @@ fun BgPhotoCard(
     textModifier: Modifier = Modifier,
 ) {
     Card(
-        modifier = modifier
-            .size(200.dp)
-            .padding(0.dp)
-            .background(Color.Gray),
+        modifier = modifier.size(200.dp),
         elevation = 4.dp,
         contentColor = MaterialTheme.colors.primary,
     ) {
+        val randomColor by remember { mutableStateOf(HideAndChessCell.Color.values().random()) }
+        val backgroundColor = when (randomColor) {
+            HideAndChessCell.Color.PURPLE -> Purple
+            HideAndChessCell.Color.ORANGE -> Orange
+            HideAndChessCell.Color.BLUE -> Blue
+            HideAndChessCell.Color.GREEN -> Green
+            HideAndChessCell.Color.GRAY -> Gray
+            HideAndChessCell.Color.PINK -> Pink
+            HideAndChessCell.Color.CIEL -> Ciel
+            HideAndChessCell.Color.RED -> Red
+            HideAndChessCell.Color.BROWN -> Brown
+            HideAndChessCell.Color.YELLOW -> Yellow
+        }
+
         Column(
-            modifier = textModifier.padding(0.dp),
+            modifier = textModifier.padding(0.dp).background(backgroundColor),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
--- src/jvmMain/kotlin/ui/EnumExt.kt
+++ src/jvmMain/kotlin/ui/EnumExt.kt
@@ -1,6 +1,6 @@
 package ui
 
-inline fun <reified T: Enum<T>> T.next(): T {
+inline fun <reified T : Enum<T>> T.next(): T {
     val values = enumValues<T>()
     val nextOrdinal = (ordinal + 1) % values.size
     return values[nextOrdinal]
